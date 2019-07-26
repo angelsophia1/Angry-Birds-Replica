0274b152a71b4a736b439692531c555834974a2f
angelsophia1 <angelsunyulei@126.com>
2019-07-26 13:08:40 +0800

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Ball : MonoBehaviour {
+    public Rigidbody2D rb;
+    public Rigidbody2D hook;
+    public GameObject nextBall;
+    public float maxDragDistance = 2f;
+    public float releaseTime = .15f;
+    private bool isPressed = false;
+    private void Update()
+    {
+        if (isPressed)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector2.Distance(mousePos, hook.position) > maxDragDistance)
+            {
+                rb.position = hook.position + (mousePos - hook.position).normalized * maxDragDistance;
+            }
+            else
+            {
+                rb.position = mousePos;
+            }
+        }
+    }
+    private void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+    private void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+        StartCoroutine(Release());
+    }
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseTime);
+        GetComponent<SpringJoint2D>().enabled = false;
+        this.enabled = false;
+        yield return new WaitForSeconds(2f);
+        if (nextBall != null && Enemy.enemyCount > 0)
+        {
+            nextBall.SetActive(true);
+        }
+        else
+        {
+            yield return new WaitForSeconds(1f);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Enemy : MonoBehaviour {
+    public float health = 4f;
+    public GameObject deathEffect;
+    public static int enemyCount = 0;
+    private void Start()
+    {
+        enemyCount++;
+    }
+    private void OnCollisionEnter2D(Collision2D colinfo)
+    {
+        if (colinfo.relativeVelocity.magnitude > health)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+        Instantiate(deathEffect,transform.position,Quaternion.identity);
+
+        enemyCount--;
+        if(enemyCount<= 0)
+        {
+            Debug.Log("Level Won!");
+        }
+    }
+
+}
+
